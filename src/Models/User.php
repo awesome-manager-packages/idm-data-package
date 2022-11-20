31f38d3d907432ab2213304691ef9402ebcea33a
Lebedev Vadim <v.lebedev@qsoft.ru>
2022-11-21 02:19:28 +0800

    Add roles to user

--- src/Models/User.php
+++ src/Models/User.php
@@ -24,4 +24,61 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
     protected $hidden = [
         'password',
     ];
+
+    public function getRoles(): array
+    {
+        return $this->prepareRoles($this->roles);
+    }
+
+    private function prepareRoles(array $roles): array
+    {
+        if (empty($roles)) {
+            return [];
+        }
+
+        $result = [];
+
+        foreach ($roles as $role) {
+            $result[] = $this->prepareRole($role);
+        }
+
+        return $result;
+    }
+
+    private function prepareRole(array $role): array
+    {
+        if (empty($role)) {
+            return [];
+        }
+
+        $res = [
+            'id' => $role['id'],
+            'name' => $role['name'],
+            'code' => $role['code'],
+            'entity_type' => $role['pivot']['entity_type'],
+            'entity_id' => $role['pivot']['entity_id'],
+        ];
+
+        $accessGroupPages = $this->prepareAccessGroupsPages($role['access_groups']);
+
+        return array_merge($res, ['access_pages' => $accessGroupPages]);
+    }
+
+    private function prepareAccessGroupsPages(array $accessGroups): array
+    {
+        if (empty($accessGroups)) {
+            return [];
+        }
+
+        $accessGroupPages = [];
+        foreach ($accessGroups as $accessGroup) {
+            if (!empty($accessGroup['access_group_pages'])) {
+                foreach ($accessGroup['access_group_pages'] as $accessGroupPage) {
+                    $accessGroupPages[] = $accessGroupPage['site_page_code'];
+                }
+            }
+        }
+
+        return $accessGroupPages;
+    }
 }
